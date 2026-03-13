fe986c0d0b44e36a92254071bd7a5bd2ffbf68d8
King Tolentino <kingtolentino0804@gmail.com>
2026-03-13 10:46:05 +0800

    update

--- views/modals/edit_delivery_modal.php
+++ views/modals/edit_delivery_modal.php
@@ -1,39 +1,152 @@
-<div class="modal fade" id="editDeliveryModal">
-
-    <div class="modal-dialog modal-lg">
-
+<div class="modal fade" id="editDeliveryModal" tabindex="-1">
+    <div class="modal-dialog modal-xl modal-dialog-centered">
         <div class="modal-content">
 
-            <div class="modal-header">
-                <h5>Edit Delivery Receipt</h5>
-            </div>
+            <form class="auto-loading-form"
+                id="editDeliveryForm"
+                action="../controllers/stock_controller.php?action=update_delivery"
+                method="POST">
 
-            <div class="modal-body">
+                <!-- Hidden fields -->
+                <input type="hidden" name="delivery_receipt_id" id="edit_delivery_receipt_id">
 
-                <label>Enter DR Number</label>
+                <div class="modal-header">
+                    <h5 class="modal-title">
+                        <i class="fa fa-edit me-2 text-warning"></i>
+                        Edit Delivery Receipt
+                    </h5>
 
-                <div class="input-group">
+                    <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+                </div>
 
-                    <input type="text" id="edit_dr_number"
-                        class="form-control"
-                        placeholder="Example: 37241">
+                <div class="modal-body">
 
-                    <button class="btn btn-primary"
-                        id="loadDRBtn">
+                    <div id="editDeliveryError" class="alert alert-danger d-none"></div>
 
-                        Load
+                    <div class="row">
+
+                        <!-- Warehouse -->
+                        <div class="col-md-6 mb-3">
+
+                            <label class="form-label">Warehouse</label>
+
+                            <select class="form-control" disabled>
+
+                                <?php
+                                mysqli_data_seek($warehouses, 0);
+                                while ($w = mysqli_fetch_assoc($warehouses)): ?>
+
+                                    <option value="<?= $w['warehouse_id'] ?>">
+                                        <?= htmlspecialchars($w['warehouse_name']) ?>
+                                    </option>
+
+                                <?php endwhile; ?>
+
+                            </select>
+
+                            <input type="hidden" name="warehouse_id" value="1">
+
+                        </div>
+
+                        <!-- Delivery Receipt -->
+                        <div class="col-md-6 mb-3">
+
+                            <label class="form-label">Delivery Receipt (DR)</label>
+
+                            <div class="input-group">
+
+                                <input type="text"
+                                    name="dr_number"
+                                    id="edit_dr_number"
+                                    class="form-control"
+                                    placeholder="Example: 23741"
+                                    required
+                                    autocomplete="off"
+                                    pattern="\d+"
+                                    title="Please enter a valid DR number (digits only)">
+
+                                <button type="button"
+                                    class="btn btn-secondary"
+                                    id="loadDRBtn">
+
+                                    <i class="fa fa-search"></i> Load
+
+                                </button>
+
+                            </div>
+
+                        </div>
+
+                        <h6 class="mt-3">Delivery Items</h6>
+
+                        <div class="items-container">
+
+                            <table class="table table-bordered delivery-table"
+                                id="editItemsTable">
+
+                                <thead>
+                                    <tr>
+                                        <th style="width:25%">Product</th>
+                                        <th style="width:10%">Qty</th>
+                                        <th style="width:10%">Unit</th>
+                                        <th style="width:15%">Total Weight (kg)</th>
+                                        <th style="width:15%">Price / Kg</th>
+                                        <th style="width:15%">Total Amount</th>
+                                        <th style="width:5%"></th>
+                                    </tr>
+                                </thead>
+
+                                <tbody>
+
+                                    <!-- Rows will be inserted via JS -->
+
+                                </tbody>
+
+                            </table>
+
+                        </div>
+
+                        <button type="button"
+                            class="btn btn-secondary"
+                            id="editAddRow">
+
+                            + Add Item
+
+                        </button>
+
+                        <div class="col-md-12 mb-3">
+
+                            <label class="form-label">Receipt Image (Optional)</label>
+
+                            <input type="file"
+                                name="receipt_image"
+                                class="form-control"
+                                accept="image/*">
+
+                            <small class="text-muted">
+                                Upload photo of delivery receipt (optional)
+                            </small>
+
+                        </div>
+
+                    </div>
+
+                </div>
+
+                <div class="modal-footer">
+
+                    <button type="submit"
+                        class="btn btn-warning w-100"
+                        id="updateDeliveryBtn">
+
+                        <i class="fa fa-save me-1"></i>
+                        Update Delivery
 
                     </button>
 
                 </div>
 
-                <hr>
-
-                <div id="editDRContainer">
-                    <!-- DR items will appear here -->
-                </div>
-
-            </div>
+            </form>
 
         </div>
     </div>
