23fe475c3bf556c3758b90e87e1936631e5ee2fb
King Tolentino <kingtolentino0804@gmail.com>
2026-03-15 01:11:21 +0800

    report issue modal

--- views/modals/view_pallet_modal.php
+++ views/modals/view_pallet_modal.php
@@ -1,12 +1,15 @@
-<div class="modal fade" id="viewPalletModal">
+<div class="modal fade" id="viewPalletModal" tabindex="-1">
 
     <div class="modal-dialog modal-lg">
 
         <div class="modal-content">
 
+            <!-- Header -->
+
             <div class="modal-header">
 
                 <h5 class="modal-title">
+                    <i class="fa fa-layer-group me-2"></i>
                     Pallet <span id="palletCodeTitle"></span>
                 </h5>
 
@@ -14,22 +17,58 @@
 
             </div>
 
+
+            <!-- Body -->
+
             <div class="modal-body">
+
+                <!-- Pallet Summary -->
+
+                <div class="row mb-3 text-center">
+
+                    <div class="col">
+
+                        <div class="metric">
+                            Boxes
+                            <br>
+                            <strong id="palletBoxCount">0</strong>
+                        </div>
+
+                    </div>
+
+                    <div class="col">
+
+                        <div class="metric">
+                            Total Weight
+                            <br>
+                            <strong><span id="palletTotalWeight">0</span> kg</strong>
+                        </div>
+
+                    </div>
+
+                </div>
+
+
+                <!-- Table -->
 
                 <div class="table-responsive">
 
-                    <table class="table table-sm">
+                    <table class="table table-sm align-middle">
 
-                        <thead>
+                        <thead class="table-light">
+
                             <tr>
                                 <th>Product</th>
-                                <th>Weight</th>
+                                <th>Weight (kg)</th>
                                 <th>Batch</th>
                                 <th>Expiry</th>
                             </tr>
+
                         </thead>
 
                         <tbody id="palletBoxesContainer">
+
+                            <!-- Filled dynamically by JS -->
 
                         </tbody>
 
