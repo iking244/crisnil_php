15155e471764340c30a2b21b3a1c90e6fe01356d
King Tolentino <kingtolentino0804@gmail.com>
2026-03-15 00:24:01 +0800

    report issue modal

--- views/warehouse_receiving.php
+++ views/warehouse_receiving.php
@@ -43,6 +43,13 @@ include "../controllers/warehouse_controller.php";
 
             </div>
 
+            <div class="mb-3">
+
+                <a href="?view=active" class="btn btn-primary btn-sm">Active</a>
+
+                <a href="?view=completed" class="btn btn-outline-secondary btn-sm">Completed</a>
+
+            </div>
 
             <!-- KPI DASHBOARD -->
 
