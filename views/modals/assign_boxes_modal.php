b81abc61515ae2d0149797bcb2a025bac71e6aa5
King Tolentino <kingtolentino0804@gmail.com>
2026-03-14 15:13:27 +0800

    warehouse receiving page

--- views/modals/assign_boxes_modal.php
+++ views/modals/assign_boxes_modal.php
@@ -1,38 +1,73 @@
-<form class="auto-loading-form" id="assignBoxesForm"
-    method="POST"
-    action="../controllers/warehouse_controller.php?action=assign_boxes">
+<div class="modal fade" id="assignBoxesModal" tabindex="-1">
 
-    <input type="hidden" name="delivery_item_id" id="delivery_item_id">
-    <input type="hidden" name="pallet_id" id="pallet_id">
+    <div class="modal-dialog modal-xl modal-dialog-centered">
 
-    <div class="modal-body">
+        <div class="modal-content">
 
-        <label class="form-label">Product</label>
+            <form class="auto-loading-form"
+                id="assignBoxesForm"
+                method="POST"
+                action="../controllers/warehouse_controller.php?action=assign_boxes">
 
-        <input type="text" id="assign_product" class="form-control" readonly>
+                <!-- hidden fields -->
+                <input type="hidden" name="delivery_item_id" id="delivery_item_id">
+                <input type="hidden" name="pallet_id" id="pallet_id">
 
-        <table class="table table-bordered">
+                <div class="modal-header">
 
-            <thead>
+                    <h5 class="modal-title">
+                        <i class="fa fa-boxes text-warning me-2"></i>
+                        Encode Boxes
+                    </h5>
 
-                <tr>
-                    <th>Weight</th>
-                    <th>Size</th>
-                    <th>Batch</th>
-                    <th>Pallet</th>
-                    <th>Expiry</th>
-                </tr>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
 
-            </thead>
+                </div>
 
-            <tbody id="boxesContainer"></tbody>
+                <div class="modal-body">
 
-        </table>
+                    <div class="mb-3">
+                        <label class="form-label">Product</label>
+                        <input type="text" id="assign_product" class="form-control" readonly>
+                    </div>
+
+                    <table class="table table-bordered">
+
+                        <thead>
+                            <tr>
+                                <th>Weight (kg)</th>
+                                <th>Size</th>
+                                <th>Batch</th>
+                                <th>Pallet</th>
+                                <th>Expiry</th>
+                            </tr>
+                        </thead>
+
+                        <tbody id="boxesContainer">
+                            <!-- JS injects rows here -->
+                        </tbody>
+
+                    </table>
+
+                </div>
+
+                <div class="modal-footer">
+
+                    <button type="button" class="btn btn-secondary"
+                        data-bs-dismiss="modal">
+                        Cancel
+                    </button>
+
+                    <button type="submit" class="btn btn-success">
+                        <i class="fa fa-save me-1"></i>
+                        Save Boxes
+                    </button>
+
+                </div>
+
+            </form>
+
+        </div>
 
     </div>
-
-    <button type="submit" class="btn btn-success w-100">
-        Save Boxes
-    </button>
-
-</form>
+</div>
