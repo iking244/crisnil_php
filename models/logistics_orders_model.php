32ac95498cd440587d8710e886612500136f6d21
King Tolentino <kingtolentino0804@gmail.com>
2026-04-13 00:34:41 +0800

    check message clusters.php

--- models/logistics_orders_model.php
+++ models/logistics_orders_model.php
@@ -196,13 +196,7 @@ function createLogisticsOrder($conn, $warehouse_id, $client_id, $product_ids, $q
         /* =========================
            RESERVE STOCK
         ========================== */
-        reserveStock(
-            $conn,
-            $job_id,
-            $warehouse_id,
-            $product_ids,
-            $quantities
-        );
+
 
         mysqli_commit($conn);
 
