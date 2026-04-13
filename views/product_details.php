c1094cfd45fad2bc67dff54b63649e1f100ea614
King Tolentino <kingtolentino0804@gmail.com>
2026-04-14 02:22:39 +0800

    logistics_orders_model.php

--- views/product_details.php
+++ views/product_details.php
@@ -31,6 +31,7 @@ if (!$product) {
 
 // Fetch additional data (you'll need to add these functions to products_model.php)
 $stats = getProductStats($databaseconn, $product_id);
+var_dump($stats);
 if ($stats['available'] <= 0) {
     $stock_status = "Out of Stock";
 } elseif ($stats['expiring'] > 0) {
