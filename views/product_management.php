e10621335e709762830a1a2e5a94827fff8ddea2
King Tolentino <kingtolentino0804@gmail.com>
2026-04-13 12:31:54 +0800

    sweet alert fix at product management

--- views/product_management.php
+++ views/product_management.php
@@ -160,6 +160,7 @@ include "../controllers/products_controller.php";
 
 <!-- Scripts -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 <script src="../scripts/table.js"></script>
 <script src="../scripts/products.js"></script>
 <script src="../scripts/notif.js"></script>
