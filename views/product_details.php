ef375ab84e8aa301ba80e1667e8ac29262789d4c
King Tolentino <kingtolentino0804@gmail.com>
2026-05-14 16:09:37 +0800

    products_model.php

--- views/product_details.php
+++ views/product_details.php
@@ -1,6 +1,8 @@
 <?php
 // views/product_details.php
-
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 session_start();
 
 if (!isset($_SESSION['USER_ID'])) {
